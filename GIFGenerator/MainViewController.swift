512b4b69c8e755e4360852da74dc4564ed0ac7c5
Kisoon Kwon <moxie2ks@gmail.com>
2023-03-13 13:56:59 +0900

    Updated UIButtons in MainViewController.

--- GIFGenerator/MainViewController.swift
+++ GIFGenerator/MainViewController.swift
@@ -30,18 +30,29 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         
-        videoToGifButton.setTitle("Video -> GIF", for: .normal)
-        videoToGifButton.backgroundColor = .darkGray
-        videoToGifButton.setTitleColor(.lightGray, for: .normal)
-        videoToGifButton.frame = CGRect(x: 20, y: 100, width: 330, height: 52)
-        videoToGifButton.addTarget(self, action: #selector(showVideoPickerView), for: .touchUpInside)
+        self.videoToGifButton.setTitle("Video -> GIF", for: .normal)
+        self.videoToGifButton.backgroundColor = .orange
+        self.videoToGifButton.layer.cornerRadius = 15
+        self.videoToGifButton.layer.shadowColor = UIColor.gray.cgColor
+        self.videoToGifButton.layer.shadowOpacity = 1.0
+        self.videoToGifButton.layer.shadowOffset = CGSize.zero
+        self.videoToGifButton.layer.shadowRadius = 6
+        self.videoToGifButton.frame = CGRect(x: 20,
+                                        y: self.view.safeAreaInsets.top + 100,
+                                    width: self.view.frame.width - 40,
+                                   height: 52)
+        self.videoToGifButton.addTarget(self, action: #selector(showVideoPickerView), for: .touchUpInside)
         self.view.addSubview(self.videoToGifButton)
         
-        livePhotoToGifButton.setTitle("LivePhoto -> GIF", for: .normal)
-        livePhotoToGifButton.backgroundColor = .darkGray
-        livePhotoToGifButton.setTitleColor(.lightGray, for: .normal)
-        livePhotoToGifButton.frame = CGRect(x: 20, y: videoToGifButton.frame.maxY+20, width: 330, height: 52)
-        livePhotoToGifButton.addTarget(self, action: #selector(showLivePhotoPickerView), for: .touchUpInside)
+        self.livePhotoToGifButton.setTitle("LivePhoto -> GIF", for: .normal)
+        self.livePhotoToGifButton.backgroundColor = .systemPink
+        self.livePhotoToGifButton.layer.cornerRadius = 15
+        self.livePhotoToGifButton.layer.shadowColor = UIColor.gray.cgColor
+        self.livePhotoToGifButton.layer.shadowOpacity = 1.0
+        self.livePhotoToGifButton.layer.shadowOffset = CGSize.zero
+        self.livePhotoToGifButton.layer.shadowRadius = 6
+        self.livePhotoToGifButton.frame = CGRect(x: 20, y: self.videoToGifButton.frame.maxY+20, width: self.view.frame.width - 40, height: 52)
+        self.livePhotoToGifButton.addTarget(self, action: #selector(showLivePhotoPickerView), for: .touchUpInside)
         self.view.addSubview(self.livePhotoToGifButton)
     }
 
